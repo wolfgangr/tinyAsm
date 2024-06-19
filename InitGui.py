5f45519548577b4e5c4e9d288e4bdb9ea2b17fab
wrosner <wrosner@kellerkind.rosner.lokal>
2024-06-19 22:10:17 +0200

    start boilerplating

---
+++ InitGui.py
@@ -0,0 +1,62 @@
+# ***************************************************************************
+# *   Copyright (c) 2015 John Doe john@doe.com                              *   
+# *                                                                         *
+# *   This file is part of the FreeCAD CAx development system.              *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENSE text file.                                 *
+# *                                                                         *
+# *   FreeCAD is distributed in the hope that it will be useful,            *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Lesser General Public License for more details.                   *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with FreeCAD; if not, write to the Free Software        *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************/
+
+
+import FreeCAD
+import sys
+
+class MyWorkbench (Workbench):
+
+    MenuText = "My Workbench"
+    ToolTip = "A description of my workbench"
+    Icon = """paste here the contents of a 16x16 xpm icon"""
+
+    def Initialize(self):
+        """This function is executed when the workbench is first activated.
+        It is executed once in a FreeCAD session followed by the Activated function.
+        """
+        import MyModuleA, MyModuleB # import here all the needed files that create your FreeCAD commands
+        self.list = ["MyCommand1", "MyCommand2"] # a list of command names created in the line above
+        self.appendToolbar("My Commands", self.list) # creates a new toolbar with your commands
+        self.appendMenu("My New Menu", self.list) # creates a new menu
+        self.appendMenu(["An existing Menu", "My submenu"], self.list) # appends a submenu to an existing menu
+
+    def Activated(self):
+        """This function is executed whenever the workbench is activated"""
+        return
+
+    def Deactivated(self):
+        """This function is executed whenever the workbench is deactivated"""
+        return
+
+    def ContextMenu(self, recipient):
+        """This function is executed whenever the user right-clicks on screen"""
+        # "recipient" will be either "view" or "tree"
+        self.appendContextMenu("My commands", self.list) # add commands to the context menu
+
+    def GetClassName(self): 
+        # This function is mandatory if this is a full Python workbench
+        # This is not a template, the returned string should be exactly "Gui::PythonWorkbench"
+        return "Gui::PythonWorkbench"
+       
+Gui.addWorkbench(MyWorkbench())
